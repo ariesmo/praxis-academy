23b769f1e152c32dec61ec13a0d2e01329362c44
ariesmo <abiesah@gmail.com>
2019-09-05 07:46:26 +0700

    update-project

---
+++ novice/03-03/latihan/books/bin/main/example/micronaut/BooksRepositoryImpl.groovy
@@ -0,0 +1,16 @@
+package example.micronaut
+
+import groovy.transform.CompileStatic
+import javax.inject.Singleton
+
+@CompileStatic
+@Singleton
+class BooksRepositoryImpl implements BooksRepository {
+    @Override
+    List<Book> findAll(){
+        [
+            new Book("1491950358", "Building Microservice"),
+            new Book("1680502395", "Release It!"),
+        ]
+    }
+}
